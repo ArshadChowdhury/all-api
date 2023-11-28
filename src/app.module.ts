0cb4691fb39b79de79a3de63b5b0b8c7c967a6db
Md. Arshad <arshadchowdhury46@gmail.com>
2023-11-28 19:31:49 +0600

    Setup with nest js and contact route integrated

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ContactModule } from './contact/contact.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    ContactModule,
+  ],
+
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
