f09279fffd97aa0a17def9950ef569150aa65570
Md. Arshad <arshadchowdhury46@gmail.com>
2023-11-28 21:07:30 +0600

    CORS error trying to allow origin

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-app.enableCors({
-  allowedHeaders: '*',
-  origin: '*',
-  credentials: true,
-});
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
