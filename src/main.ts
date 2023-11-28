de2c8cda124edb14eb13e6b2c1482fad305dd843
Md. Arshad <arshadchowdhury46@gmail.com>
2023-11-28 20:32:33 +0600

    Configured CORS further

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://arshadchowdhury.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
