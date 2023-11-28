143ddb88ecd276d42eb246baa9697c1b2561d120
Md. Arshad <arshadchowdhury46@gmail.com>
2023-11-28 20:55:16 +0600

    Solved the CORS error locally

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://arshadchowdhury.vercel.app/'],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
